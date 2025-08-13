1731355813af9a6eb96ac6d42eb3d4913db95c7c
Matias Enriquez <matiasenriquez97@gmail.com>
2025-08-13 17:09:54 -0300

    Inicio del desarrollo del proyecto final UTN front end

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: "lightcoral" }}>
+      <p>Sitio desarrollado por Gabriel Alberini y Enriquez Matias</p>
+    </footer>
+  )
+}
+
+export { Footer }
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom"
+import { useAuth } from "../context/UserContext"
+
+const Header = () => {
+  const { user, logout } = useAuth()
+
+  const handleLogout = () => {
+    logout()
+  }
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          {/* Cambiar elementos a por componentes Link de react-router-dom */}
+          {
+            user && <>
+              <li><Link to="/">Inicio</Link></li>
+              <li><Link to="/dashboard">Dashboard</Link></li>
+              <button onClick={handleLogout}>Cerrar sesión</button>
+            </>
+          }
+          {
+            !user && <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/registrate">Registrate</Link></li>
+            </>
+          }
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export { Header }
