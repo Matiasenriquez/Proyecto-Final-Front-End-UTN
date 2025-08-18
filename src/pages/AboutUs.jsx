5c7a01752f2772c7bea7bfbe5622009cbcc7ddb6
Matias Enriquez <matiasenriquez97@gmail.com>
2025-08-18 11:41:32 -0300

    Add: se añadio estilos a la página Sobre Nosotros

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
-import { Layout } from "../components/Layout";
-import { useAuth } from "../context/UserContext";
+import { Layout } from "../components/Layout"
+import "../styles/pages/AboutUs.css"
+
 const AboutUs = () => {
   return (
     <Layout>
-      <div>
-        <h1>Sobre Nosotros</h1>
-        <section>
-          <h2>De que trata el proyecto</h2>
-        </section>
-        <section>
-          <h2>A quien esta dirigido</h2>
-        </section>
-        <section>
-          <h2>Que tecnologias se usaron</h2>
-        </section>
-      </div>
+      <section className="about-section">
+        <div className="about-cards-container">
+          <div className="about-card">
+            <h3>De qué trata el proyecto</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero unde, eaque eveniet animi sed quisquam exercitationem voluptatem reiciendis impedit officia quidem nostrum asperiores ullam esse, assumenda ducimus laborum quo at!</p>
+          </div>
+
+          <div className="about-card">
+            <h3>A quién está dirigido</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, accusantium eveniet? Quaerat beatae mollitia illum perferendis, sed laudantium consectetur vero accusamus velit reprehenderit officia consequuntur ducimus ratione quasi ipsam error.</p>
+          </div>
+
+          <div className="about-card">
+            <h3>Qué tecnologías se usaron</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia earum dolore reprehenderit, non impedit molestias, aut facilis quia consequuntur minus iusto enim, numquam voluptatibus. Ratione tempora placeat exercitationem hic laborum!</p>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
+
 export { AboutUs }
